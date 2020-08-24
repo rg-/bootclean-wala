ff86e66ae38c01a46e7afbd07c771a674f047977
rg- <roberto@rgdesign.org>
2020-08-24 13:50:16 -0300

    Update layout-checkout.php

--- functions/woocommerce/layout-checkout.php
+++ functions/woocommerce/layout-checkout.php
@@ -17,9 +17,10 @@ function wpbc_checkout_template_redirect(){
 
 	// When on the checkout with an empty cart, redirect to cart page.
 	if ( is_page( wc_get_page_id( 'checkout' ) ) && wc_get_page_id( 'checkout' ) !== wc_get_page_id( 'cart' ) && WC()->cart->is_empty() && empty( $wp->query_vars['order-pay'] ) && ! isset( $wp->query_vars['order-received'] ) && ! is_customize_preview() && apply_filters( 'woocommerce_checkout_redirect_empty_cart', true ) ) {
-		wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ), 'notice' );
+		
 		$ordenar_page_id = WPBC_get_theme_settings('general_post_object_ordenar');
 		if( !is_wc_endpoint_url('order-received') && !isset($_GET['key']) ){
+			wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ), 'notice' );
 			wp_safe_redirect( get_the_permalink($ordenar_page_id) );
 			exit;
 		}
